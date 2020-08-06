98624ad4efc38c09a7aec182a8ec0602d8dac9b2
isabelpt <56853689+isabelpt@users.noreply.github.com>
2020-08-05 22:44:54 -0400

    version 1

---
+++ MiniProject1/MiniProject1/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MiniProject1
+//
+//  Created by Isabel Prado-Tucker on 8/5/20.
+//  Copyright © 2020 Isabel Prado-Tucker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var answer1: UILabel!
+    @IBOutlet weak var answer2: UILabel!
+    @IBOutlet weak var answer3: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func button1(_ sender: UIButton) {
+    }
+    @IBAction func button2(_ sender: UIButton) {
+    }
+    @IBAction func button3(_ sender: UIButton) {
+    }
+}
+
